13ccc5c53b4c10adbc3ef2cc321df4074854f160
debian <debian@debian10Server.lapro.local>
2021-11-22 11:59:39 +0100

    Update

--- Langage C/Chapitre 1/TP 1/exercice 5.cpp
+++ Langage C/Chapitre 1/TP 1/exercice 5.cpp
@@ -17,14 +17,17 @@ int main()
 
     total = prix * quantite;
 
+    /*
     if(total > 500)
     {
         printf("Sous total : %.2f euros\n", total);
         total *= 0.9;
         printf("Une remise de 10 %% a ete appliquee.\n");
     }
+    */
 
-    printf("\n\nLe montant a payer est de : %.2f euros.\n\n", total);
+    //printf("\n\nLe montant a payer est de : %.2f euros.\n\n", total);
+    printf("\n\nLe montant a payer est de : %.2f euros.\n\n", total > 500 ? total * 0.9 : total);
 
     return 0;
 }
